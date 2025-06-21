10d4344906d6b2df50414b2300e7ed83963737a5
pranavkaushik-copart <contact@klikktech.com>
2025-06-21 10:39:36 -0500

    ADd base details

---
+++ src/main/java/com/klikk/urbansaathi/UrbansaathiApplication.java
@@ -0,0 +1,13 @@
+package com.klikk.urbansaathi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrbansaathiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrbansaathiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/klikk/urbansaathi/UrbansaathiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.klikk.urbansaathi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrbansaathiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
